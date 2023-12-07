97ac4e2a7d9a69e56eeb3bb41e319d773e2ea017
Churng <churng0421@gmail.com>
2023-12-07 12:16:05 +0800

    更新：錯誤提示

--- js/Login/login.js
+++ js/Login/login.js
@@ -12,62 +12,66 @@ $(function () {
 			url: `${apiURL}/adminLogin`,
 			data: { account: account, password: password, chsm: chsm },
 			success: function (response) {
-				$("#loginResult").html("服务器响应：" + response);
+				if (response.returnCode === "1") {
+					$("#loginResult").html("服务器响应：" + response);
 
-				const returnCode = response.returnCode;
-				const returnMessage = response.returnMessage;
-				const returnData = response.returnData.sessionId;
+					const returnCode = response.returnCode;
+					const returnMessage = response.returnMessage;
+					const returnData = response.returnData.sessionId;
 
-				const userData = {
-					returnCode: returnCode,
-					returnMessage: returnMessage,
-					sessionId: returnData,
-				};
+					const userData = {
+						returnCode: returnCode,
+						returnMessage: returnMessage,
+						sessionId: returnData,
+					};
 
-				const jsonString = JSON.stringify(userData);
+					const jsonString = JSON.stringify(userData);
 
-				localStorage.setItem("userData", jsonString);
+					localStorage.setItem("userData", jsonString);
 
-				const jsonStringFromLocalStorage = localStorage.getItem("userData");
-				const gertuserData = JSON.parse(jsonStringFromLocalStorage);
-				const getSessionId = userData.sessionId;
+					const jsonStringFromLocalStorage = localStorage.getItem("userData");
+					const gertuserData = JSON.parse(jsonStringFromLocalStorage);
+					const getSessionId = userData.sessionId;
 
-				var action = "select";
-				var chsmtoMenu = getSessionId + action + "HBAdminMenuApi";
-				var chsm = CryptoJS.MD5(chsmtoMenu).toString().toLowerCase();
+					var action = "select";
+					var chsmtoMenu = getSessionId + action + "HBAdminMenuApi";
+					var chsm = CryptoJS.MD5(chsmtoMenu).toString().toLowerCase();
 
-				$.ajax({
-					type: "POST",
-					url: `${apiURL}/menu`,
-					data: { session_id: getSessionId, action: action, chsm: chsm },
-					success: function (secondApiResponse) {
-						console.log(secondApiResponse);
+					$.ajax({
+						type: "POST",
+						url: `${apiURL}/menu`,
+						data: { session_id: getSessionId, action: action, chsm: chsm },
+						success: function (secondApiResponse) {
+							console.log(secondApiResponse);
 
-						var currentUser = {
-							userreturnCode: secondApiResponse.returnCode,
-							userName: secondApiResponse.name,
-							userPhoto: secondApiResponse.personalPhoto,
-							userretrunData: secondApiResponse.returnData,
-							userMessage: secondApiResponse.returnMessage,
-						};
+							var currentUser = {
+								userreturnCode: secondApiResponse.returnCode,
+								userName: secondApiResponse.name,
+								userPhoto: secondApiResponse.personalPhoto,
+								userretrunData: secondApiResponse.returnData,
+								userMessage: secondApiResponse.returnMessage,
+							};
 
-						localStorage.setItem("currentUser", JSON.stringify(currentUser));
+							localStorage.setItem("currentUser", JSON.stringify(currentUser));
 
-						// 成功導向測試頁面
-						if (currentUser.userreturnCode === "1") {
-							showSuccessLoginNotification();
+							// 成功導向測試頁面
+							if (currentUser.userreturnCode === "1") {
+								showSuccessLoginNotification();
 
-							setTimeout(function () {
-								window.location.href = "index.html";
-							}, 1000);
-						} else {
-							handleApiResponse(secondApiResponse);
-						}
-					},
-					error: function () {
-						showErrorNotification();
-					},
-				});
+								setTimeout(function () {
+									window.location.href = "index.html";
+								}, 1000);
+							} else {
+								handleApiResponse(secondApiResponse);
+							}
+						},
+						error: function () {
+							showErrorNotification();
+						},
+					});
+				} else {
+					handleApiResponse(secondApiResponse);
+				}
 			},
 			error: function () {
 				showErrorNotification();
